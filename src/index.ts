503efff1a8a7ed08889f44b4b8f7567a6068f03e
Arturo-Blanco <103155759+Arturo-Blanco@users.noreply.github.com>
2022-05-28 09:29:18 -0300

    Se agrega length y se hace correciones para legibilidad

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,19 @@
 let v1: number[] = new Array(6);
-let valor1: number, i: number;
+let v2: number[] = new Array(6);
+let vSuma: number[] = new Array(6);
+let valor1: number, valor2: number, i: number; 
 
-for (i = 0; i < 6; i++) {// lo ideal seria usar v1.length ya que si necesitas cambiar la cantidad de elementos solo cambias en la primer asignacion y no en el resto del codigo.
-  valor1 = Number(prompt("Ingrese valor " + (i + 1) + " del primer arreglo"));
+for (i = 0; i < v1.length; i++) {
+  valor1 = Number(prompt("Ingrese valor "+ (i + 1) + " del primer arreglo"));
   v1[i] = valor1;
 }
-
-let v2: number[] = new Array(6);
-let valor2: number;
-for (i = 0; i < 6; i++) {
+for (i = 0; i < v2.length; i++) {
   valor2 = Number(prompt("Ingrese valor " + (i + 1) + " del segundo arreglo"));
   v2[i] = valor2;
 }
-let vSuma: number[] = [];
-for (i = 0; i < 6; i++) {
+for (i = 0; i < vSuma.length ; i++) {
   vSuma[i] = v1[i] + v2[i];
 }
 console.log("Los numeros del primer arreglo son: " + v1);
 console.log("Los numeros del segundo arreglo son: " + v2);
 console.log("La suma de cada elemento del arreglo son " + vSuma);
-//Está muy bien tu solucion
